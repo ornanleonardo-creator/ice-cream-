1ead0b3d2e730b90701bfd2db8dfad5b95ad3e4f
ornanleonardo-creator <ornanleonardo@gmail.com>
2026-02-01 20:53:07 +0200

    Create VerifyWorksActivity.kt

---
+++ app/src/main/java/org/banza/ocr/VerifyWorksActivity.kt
@@ -0,0 +1,11 @@
+package org.banza.ocr
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class VerifyWorksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verify)
+    }
+}
