79b5b8c80dcb27ed8248101a77c17f483f9ab4de
ornanleonardo-creator <ornanleonardo@gmail.com>
2026-02-01 10:29:14 +0200

    Update MainActivity.kt

--- app/src/main/java/org/banza/ocr/MainActivity.kt
+++ app/src/main/java/org/banza/ocr/MainActivity.kt
@@ -3,28 +3,24 @@ package org.banza.ocr
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var previewView: PreviewView
     private val CAMERA_REQUEST_CODE = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        previewView = PreviewView(this)
-        setContentView(previewView)
+        val textView = TextView(this)
+        textView.text = "App lancée avec succès"
+        textView.textSize = 20f
+        setContentView(textView)
 
-        if (hasCameraPermission()) {
-            startCamera()
-        } else {
+        if (!hasCameraPermission()) {
             requestCameraPermission()
         }
     }
@@ -42,40 +38,5 @@ class MainActivity : AppCompatActivity() {
             arrayOf(Manifest.permission.CAMERA),
             CAMERA_REQUEST_CODE
         )
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if (requestCode == CAMERA_REQUEST_CODE &&
-            grantResults.isNotEmpty() &&
-            grantResults[0] == PackageManager.PERMISSION_GRANTED
-        ) {
-            startCamera()
-        }
-    }
-
-    private fun startCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-
-        cameraProviderFuture.addListener({
-            val cameraProvider = cameraProviderFuture.get()
-
-            val preview = Preview.Builder().build()
-            preview.setSurfaceProvider(previewView.surfaceProvider)
-
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            cameraProvider.bindToLifecycle(
-                this,
-                cameraSelector,
-                preview
-            )
-
-        }, ContextCompat.getMainExecutor(this))
     }
 }
