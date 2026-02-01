0a19ec1f4659292ca949af8476da6eba915d104c
ornanleonardo-creator <ornanleonardo@gmail.com>
2026-02-01 20:52:27 +0200

    Create ScannerActivity.kt

---
+++ app/src/main/java/org/banza/ocr/ScannerActivity.kt
@@ -0,0 +1,11 @@
+package org.banza.ocr
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ScannerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scanner)
+    }
+}
